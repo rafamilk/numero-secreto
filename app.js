7f867c4eda06487e2ec0c691cc2868518af0ddb9
rafamilk <rafadesenhista@yahoo.com.br>
2024-03-11 17:36:09 -0300

    Revert "alterando limite para 100"
    This reverts commit 4fdb7a7ef51f63c54a970e27b2a2d45f0d6da0b1.

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 let listaDeNumerosSorteados = [];
+<<<<<<< HEAD
 let numeroLimite = 50;
+=======
+let numeroLimite = 10;
+>>>>>>> parent of 4fdb7a7 (alterando limite para 100)
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
